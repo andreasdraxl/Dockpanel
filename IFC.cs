0af8b3aa7c740d157b601ca95f9010e05dd51e01
Andreas Draxl <andreas_draxl_@live.de>
2024-07-19 22:55:55 +0200

    Add files via upload
    Where should i start.  Mainly i want to create a docking panel. Based on a udemy tutorial...       i would be happy about any help and advice. the solution is still not working. First aim is to click on an element and getting the ID.

---
+++ IFC.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.Attributes;
+
+namespace IFC
+{
+    internal class IFC: IExternalApplication
+    {
+        private static IFC dPanel;
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RegisterDockablePanels(application);
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+        public Result RegisterDockablePanels(UIControlledApplication application)
+        {
+            dPanel = new IFC();
+            DockablePaneId dockId = new DockablePaneId(new Guid("118DFA3E-C4C6-4BE9-985B-76F645D0F6D1"));
+            try
+            {
+                application.RegisterDockablePane(dockId, "DockPane", dPanel as IDockablePaneProvider);
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error Message", ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+        [Transaction(TransactionMode.Manual)]
+        public class ShowDockPane: IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                try
+                {
+                    DockablePaneId id = new DockablePaneId(new Guid("118DFA3E-C4C6-4BE9-985B-76F645D0F6D1"));
+                    DockablePane dockableWindow = commandData.Application.GetDockablePane(id);
+                    dockableWindow.Show();
+                }
+                catch(Exception ex)
+                {
+                    TaskDialog.Show("Info Message", ex.Message);
+                }
+                return Result.Succeeded;
+            }
+
+        }
+
+    }
+}
---
+++ IFC.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI.Selection;
+
+namespace IFC
+{
+    /// <summary>
+    /// Interaction logic for IFC.xaml
+    /// </summary>
+    public partial class IFC : Page, IDockablePaneProvider
+    {
+        public IFC()
+        {
+            InitializeComponent();
+        }
+
+        public void SetupDockablePane(DockablePaneProviderData data)
+        {
+            data.FrameworkElement = this as FrameworkElement;
+            data.InitialState = new DockablePaneState
+            {
+                DockPosition = DockPosition.Tabbed,
+                TabBehind = DockablePanes.BuiltInDockablePanes.ProjectBrowser
+            };
+            lbId.Content = "LOI 500";
+        }
+
+    }
+}
